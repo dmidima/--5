93109b70566c32c4d4c8fef291d40b2caef46d8f
Oparin Dmitry <dmitry40646@gmail.com>
2024-11-10 01:21:52 +0300

    npm

---
+++ run.js
@@ -0,0 +1,6 @@
+const myModule = require('my-lodash-module');
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const filteredNumbers = myModule.myModuleFunction(numbers, 5);
+
+console.log(filteredNumbers);
